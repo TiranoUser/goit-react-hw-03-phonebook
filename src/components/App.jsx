3df8ed5c804d9bf7d9a129d71556a29a8dc0701a
TiranoUser <v130561@gmail.com>
2023-11-06 22:32:50 +0100

    first2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,30 @@
+import React, { Component } from 'react';
+
 export class App extends Component {
   state = {
     contacts: [],
-    filter: '',
+    name: '',
   };
 
   render() {
-    return <div>Test</div>;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </form>
+      </div>
+    );
   }
 }
